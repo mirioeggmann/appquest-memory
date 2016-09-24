f10d13fd2f31563fee9d6e1bc45707a2ddced92f
Mirio Eggmann <Mirio Eggmann>
2016-09-24 16:46:34 +0200

    Initial commit.

---
+++ app/src/main/java/ch/jmelab/appquest_memory/MemoryActivity.java
@@ -0,0 +1,13 @@
+package ch.jmelab.appquest_memory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MemoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_memory);
+    }
+}
